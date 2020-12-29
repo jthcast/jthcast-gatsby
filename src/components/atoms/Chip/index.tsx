c55f5b99e6b2851fc5b91e0338001fed67ee571d
jthcast <jthcast@gmail.com>
2020-12-30 00:01:01 +0900

    Fix chip children type

--- src/components/atoms/Chip/index.tsx
+++ src/components/atoms/Chip/index.tsx
@@ -5,7 +5,7 @@ import Button from '../Button';
 
 interface ChipProps {
   allowClose?: boolean;
-  children?: React.ReactElement | Array<any>;
+  children?: React.ReactElement | Array<any> | string;
   className?: string;
   onClose?: () => void;
   ghost?: boolean;
