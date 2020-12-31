70181b9ac025f26dba7ff0ad546811fe25b872da
jthcast <jthcast@gmail.com>
2021-01-01 02:29:02 +0900

    Fix markdown to mdx

--- src/pages/portfolios/index.tsx
+++ src/pages/portfolios/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Img, { FixedObject, FluidObject } from "gatsby-image";
+import GatsbyImage, { FixedObject, FluidObject } from "gatsby-image";
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import { useTranslation } from 'react-i18next';
 import { IconGithub, IconPlay } from '../../components/atoms/Icons';
@@ -32,7 +32,7 @@ export interface PortfoliosProps {
 const Portfolios = (): React.ReactElement => {
   const query = useStaticQuery(graphql`
     query AllPortfolios{
-      allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}, filter: {fileAbsolutePath: {regex: "/(content/portfolios)/"}, frontmatter: {visible: {eq: true}}}) {
+      allMdx(sort: {fields: [frontmatter___date], order: DESC}, filter: {fileAbsolutePath: {regex: "/(content/portfolios)/"}, frontmatter: {visible: {eq: true}}}) {
         nodes {
           excerpt
           frontmatter {
@@ -41,7 +41,7 @@ const Portfolios = (): React.ReactElement => {
             description
             image {
               childImageSharp {
-                fluid {
+                fluid(maxWidth: 1160, maxHeight: 500, quality: 90) {
                   ...GatsbyImageSharpFluid_withWebp_noBase64
                 }
               }
@@ -59,73 +59,71 @@ const Portfolios = (): React.ReactElement => {
       }
     }
   `);
-  const portfolios: Array<PortfoliosProps> = query.allMarkdownRemark.nodes;
+  const portfolios: Array<PortfoliosProps> = query.allMdx.nodes;
   const { t } = useTranslation();
   return (
     <Layout title={t('Portfolios.title')} description={t('Portfolios.description')}>
-      <section className="jth-portfolios">
-        <div className="jth-container">
-          {portfolios.length === 0 && (
-            <div className="jth-portfolios-nothing">
-              <h1>{t('Portfolios.portfolioDoesntExistMessage')}</h1>
-            </div>
-          )}
-          {portfolios.length > 0 && (
-            <ul className="jth-portfolios-list">
-              {portfolios.map((portfolio) => {
-                return (
-                  <li key={portfolio.fields.slug}>
-                    <article className="jth-portfolios-item">
-                      {portfolio.frontmatter.image && portfolio.frontmatter.detailPage && (
+      <section className="jth-portfolios jth-container">
+        {portfolios.length === 0 && (
+          <div className="jth-portfolios-nothing">
+            <h1>{t('Portfolios.portfolioDoesntExistMessage')}</h1>
+          </div>
+        )}
+        {portfolios.length > 0 && (
+          <ul className="jth-portfolios-list">
+            {portfolios.map((portfolio) => {
+              return (
+                <li key={portfolio.fields.slug}>
+                  <article className="jth-portfolios-item">
+                    {portfolio.frontmatter.image && portfolio.frontmatter.detailPage && (
+                      <Link
+                        to={portfolio.fields.slug}
+                        aria-label={portfolio.frontmatter.title}
+                      >
+                        <GatsbyImage fluid={portfolio.frontmatter.image.childImageSharp.fluid} alt={portfolio.frontmatter.title} />
+                      </Link>
+                    )}
+                    <div className="jth-portfolios-item-info">
+                      {portfolio.frontmatter.title && portfolio.frontmatter.detailPage ? (
                         <Link
                           to={portfolio.fields.slug}
                           aria-label={portfolio.frontmatter.title}
                         >
-                          <Img fluid={portfolio.frontmatter.image.childImageSharp.fluid} alt={portfolio.frontmatter.title} />
+                          <h2>{portfolio.frontmatter.title}</h2>
                         </Link>
-                      )}
-                      <div className="jth-portfolios-item-info">
-                        {portfolio.frontmatter.title && portfolio.frontmatter.detailPage ? (
-                          <Link
-                            to={portfolio.fields.slug}
-                            aria-label={portfolio.frontmatter.title}
+                      ) : (
+                          <h2>{portfolio.frontmatter.title}</h2>
+                        )}
+                      <div className="jth-portfolios-item-info-links">
+                        {portfolio.frontmatter.github && (
+                          <a
+                            href={portfolio.frontmatter.github}
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="github"
                           >
-                            <h2>{portfolio.frontmatter.title}</h2>
-                          </Link>
-                        ) : (
-                            <h2>{portfolio.frontmatter.title}</h2>
-                          )}
-                        <div className="jth-portfolios-item-info-links">
-                          {portfolio.frontmatter.github && (
-                            <a
-                              href={portfolio.frontmatter.github}
-                              target="_blank"
-                              rel="noreferrer"
-                              aria-label="github"
-                            >
-                              <IconGithub />
-                            </a>
-                          )}
-                          {portfolio.frontmatter.demo && (
-                            <a
-                              href={portfolio.frontmatter.demo}
-                              target="_blank"
-                              rel="noreferrer"
-                              aria-label="github"
-                            >
-                              <IconPlay className="jth-portfolios-item-info-play" />
-                            </a>
-                          )}
-                        </div>
+                            <IconGithub />
+                          </a>
+                        )}
+                        {portfolio.frontmatter.demo && (
+                          <a
+                            href={portfolio.frontmatter.demo}
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="github"
+                          >
+                            <IconPlay className="jth-portfolios-item-info-play" />
+                          </a>
+                        )}
                       </div>
-                      <p>{portfolio.frontmatter.description}</p>
-                    </article>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-        </div>
+                    </div>
+                    <p>{portfolio.frontmatter.description}</p>
+                  </article>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </section>
     </Layout>
   );
