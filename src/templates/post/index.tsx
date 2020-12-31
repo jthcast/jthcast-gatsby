53116af8b232ded91d417008b512228eb337843f
jthcast <jthcast@gmail.com>
2020-12-31 20:55:25 +0900

    Fix postTemplate

--- src/templates/post/index.tsx
+++ src/templates/post/index.tsx
@@ -1,105 +1,157 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link, graphql, PageProps } from 'gatsby';
 import Layout from '../../components/layout';
-import { MarkdownRemark } from '../../graphql-types';
 import './Post.scss';
+import { useTranslation } from 'react-i18next';
+import GatsbyImage from 'gatsby-image';
+import Card from '../../components/atoms/Card';
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { Mdx } from '../../graphql-types';
 
-type blogPostTemplateProps = {
-  site: {
-    siteMetadata: {
-      title: string
+
+export const pageQuery = graphql`
+  query createPostPage(
+    $id: String!
+    $series: String
+    $tag: [String]
+  ) {
+    mdx (id: { eq: $id }) {
+      excerpt(pruneLength: 160)
+      body
+      frontmatter {
+        title
+        date(formatString: "YYYY-MM-DD")
+        description
+        image {
+          childImageSharp {
+            fluid(maxWidth: 1160, maxHeight: 500, quality: 90) {
+              ...GatsbyImageSharpFluid_withWebp_noBase64
+            }
+          }
+        }
+        series
+        tags
+      }
     }
-  },
-  markdownRemark: MarkdownRemark,
-  previous: MarkdownRemark,
-  next: MarkdownRemark,
+    series: allMdx(filter: {frontmatter: {visible: { eq: true }, series: {eq: $series, ne: null }}, fileAbsolutePath: {regex: "/(content/posts)/"}}) {
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+        }
+      }
+    }
+    related: allMdx(filter: {frontmatter: {visible: {eq: true}, tags: {in: $tag}}, fileAbsolutePath: {regex: "/(content/posts)/"}, id: {ne: $id}}, limit: 2) {
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          image {
+            childImageSharp {
+              fluid(maxWidth: 1160, maxHeight: 500, quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp_noBase64
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+interface PostDataProps {
+  mdx?: Mdx;
+  series?: {
+    nodes: Mdx[];
+  }
+  related?: {
+    nodes: Mdx[];
+  }
 }
 
-const BlogPostTemplate = ({ data }: PageProps<blogPostTemplateProps>) => {
-  const post = data.markdownRemark;
-  const { previous, next } = data;
+const Post = ({ data }: PageProps<PostDataProps>): React.ReactElement => {
+  const { t } = useTranslation();
+  const post = data.mdx;
+  const series = data.series.nodes;
+  const related = data.related.nodes;
 
   return (
     <Layout title={post.frontmatter.title}
       description={post.frontmatter.description || post.excerpt}>
       <article
-        className="blog-post"
+        className="jth-container jth-post"
         itemScope
         itemType="http://schema.org/Article"
       >
-        <header>
+        <header className="jth-post-info">
           <h1 itemProp="headline">{post.frontmatter.title}</h1>
+          {post.frontmatter.tags && <p>{post.frontmatter.tags.join(' / ')}</p>}
           <p>{post.frontmatter.date}</p>
+          {post.frontmatter.image &&
+            <div className="jth-post-info-image">
+              <GatsbyImage
+                fluid={post.frontmatter.image.childImageSharp.fluid}
+                alt={post.frontmatter.title} />
+            </div>}
+          <p className="jth-post-info-content">{post.frontmatter.description}</p>
         </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
-        <hr />
+        <section className="jth-post-content" itemProp="articleBody">
+          <MDXRenderer scope className="jth-post-content">
+            {post.body}
+          </MDXRenderer>
+        </section>
       </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
+      <nav className="jth-post-links jth-container">
+        {series.length > 1 && (
+          <section>
+            <h2>{t('Post.seriesPosts')}</h2>
+            <ol className="jth-post-seriesPosts">
+              {series.map((seriesPost) => {
+                return (
+                  <Fragment key={seriesPost.fields.slug}>
+                    {seriesPost.frontmatter.title === post.frontmatter.title ? (
+                      <li className="jth-post-seriesPosts-this">
+                        <p>{seriesPost.frontmatter.title}</p>
+                      </li>
+                    ) : (
+                        <li>
+                          <Link
+                            to={seriesPost.fields.slug}
+                            aria-label={seriesPost.frontmatter.title}
+                          >
+                            {seriesPost.frontmatter.title}
+                          </Link>
+                        </li>
+                      )}
+                  </Fragment>
+                );
+              })}
+            </ol>
+          </section>
+        )}
+        {related.length > 0 && (
+          <section>
+            <h2>{t('Post.relatedPosts')}</h2>
+            <ul className="jth-post-relatedPosts">
+              {related.map((post) => {
+                return (
+                  <li key={post.fields.slug}>
+                    <Link to={post.fields.slug} aria-label={post.frontmatter.title}>
+                      <Card item={post} />
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        )}
       </nav>
     </Layout>
   );
 };
 
-export default BlogPostTemplate;
-
-export const pageQuery = graphql`
-  query BlogPostBySlug(
-    $id: String!
-    $previousPostId: String
-    $nextPostId: String
-  ) {
-    markdownRemark(id: { eq: $id }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        description
-      }
-    }
-    previous: markdownRemark(id: { eq: $previousPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-    next: markdownRemark(id: { eq: $nextPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-  }
-`;
+export default Post;
