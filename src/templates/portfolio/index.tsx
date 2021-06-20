ce9cad4575e99a8f0b8bd0f98b6747a10453de9f
jthcast <jthcast@gmail.com>
2021-06-21 03:34:56 +0900

    Debug

--- src/templates/portfolio/index.tsx
+++ src/templates/portfolio/index.tsx
@@ -20,7 +20,6 @@ const Portfolio = ({
   const { t } = useTranslation();
   const portfolio = data.mdx;
   const related = data.series.nodes;
-  console.log('##', related);
 
   return (
     <Layout
